f601d6d0d4396383a1d43a33df330a8095280f2d
almeswe <armag2003@mail.ru>
2023-10-04 16:48:44 +0300

    symtable refactored.

--- src/nnc_symtable.c
+++ src/nnc_symtable.c
@@ -1,5 +1,20 @@
 #include "nnc_symtable.h"
 
+/**
+ * @brief String representation of identifier's context. 
+ */
+const static char* ictx_str[] = {
+    [IDENT_DEFAULT]        = "variable",
+    [IDENT_FUNCTION]       = "function",
+    [IDENT_NAMESPACE]      = "namespace",
+    [IDENT_ENUMERATOR]     = "enumerator",
+    [IDENT_FUNCTION_PARAM] = "function param"
+};
+
+/**
+ * @brief Initializes preallocated `nnc_st` instance.
+ * @param out_table Pointer to preallocated instance of `nns_st` for initialization.
+ */
 void nnc_st_init(nnc_st* out_table) {
     out_table->ctx = ST_CTX_DEFAULT;
     out_table->syms = map_init_with(8);
@@ -7,10 +22,36 @@ void nnc_st_init(nnc_st* out_table) {
     out_table->ref = (struct _nnc_st_ref){0};
 }
 
-nnc_bool nnc_st_has(const nnc_st* st, const char* key) {
+/**
+ * @brief Releases instance of `nnc_st`.
+ * @param st Pointer to `nnc_st` instance to be released.
+ */
+void nnc_st_fini(nnc_st* st) {
+    map_fini(st->syms);
+    map_fini(st->types);
+    for (nnc_u64 i = 0; i < buf_len(st->branches); i++) {
+        nnc_st_fini(st->branches[i]);
+    }
+    buf_free(st->branches);
+}
+
+/**
+ * @brief Determines if table contains specified symbol.
+ * @param st Pointer to `nnc_st` in which to check for symbol.
+ * @param key String representation of symbol.
+ * @param kind Kind of symbol.
+ * @return `true` if symbol inside of specified `st` or it's parents, `false` otherwise.
+ */
+nnc_bool nnc_st_has(const nnc_st* st, const char* key, nnc_st_sym_kind kind) {
     nnc_bool has_in_map = false;
     for (; st != NULL && !has_in_map; st = st->root) {
-        has_in_map |= map_has_s(st->syms, key);
+        switch (kind) {
+            case ST_SYM_TYPE:  has_in_map |= map_has_s(st->types, key); break; 
+            case ST_SYM_IDENT: has_in_map |= map_has_s(st->syms, key);  break; 
+            default: nnc_abort_no_ctx("nnc_st_has: unknown sym kind.");
+        }
+        // stop from searching if current `st` is namespace
+        // this is done for the purpose of isolating scope of namespace.
         if (st->ctx & ST_CTX_NAMESPACE) {
             break;
         }
@@ -18,63 +59,62 @@ nnc_bool nnc_st_has(const nnc_st* st, const char* key) {
     return has_in_map;
 }
 
-nnc_bool nnc_st_has_type(const nnc_st* st, const char* key) {
-    nnc_bool has_in_map = false;
-    for (; st != NULL && !has_in_map; st = st->root) {
-        has_in_map |= map_has_s(st->types, key);
-    }
-    return has_in_map; 
-}
-
-nnc_symbol* nnc_st_get(const nnc_st* st, const char* key) {
-    nnc_symbol* sym = NULL;
-    for (; st != NULL; st = st->root) {
-        if (map_has_s(st->syms, key)) {
-            sym = map_get_s(st->syms, key);
+/**
+ * @brief Tryes to get symbol from `st` by it's name.
+ * @param st Pointer to `nnc_st` in which to check for symbol.
+ * @param key String representation of symbol.
+ * @param kind Kind of symbol.
+ * @return In case when no symbol found, NULL is returned.
+ */
+nnc_heap_ptr nnc_st_get(const nnc_st* st, const char* key, nnc_st_sym_kind kind) {
+    nnc_heap_ptr sym = NULL;
+    for (; st != NULL && sym == NULL; st = st->root) {
+        nnc_map* map = NULL;
+        switch (kind) {
+            case ST_SYM_TYPE:  map = st->types; break;
+            case ST_SYM_IDENT: map = st->syms;  break;
+            default: nnc_abort_no_ctx("nnc_st_get: unknown sym kind.");
+        }
+        if (map_has_s(map, key)) {
+            sym = map_get_s(map, key);
+        }
+        // stop from searching if current `st` is namespace
+        // this is done for the purpose of isolating scope of namespace.
+        if (st->ctx == ST_CTX_NAMESPACE) {
+            break;
         }
     }
     return sym;
 }
 
-nnc_type* nnc_st_get_type(const nnc_st* st, const char* key) {
-    nnc_type* type = NULL;
-    for (; st != NULL; st = st->root) {
-        if (map_has_s(st->types, key)) {
-            type = map_get_s(st->types, key);
-        }
-    }
-    return type;
-}
-
-nnc_symbol* nnc_st_get_below(const nnc_st* st, const char* key) {
-    nnc_st temp_st = *st;
-    temp_st.root = NULL;
-    return nnc_st_get(&temp_st, key);
-}
-
-void nnc_st_put(nnc_st* st, nnc_symbol* sym) {
-    const static char* ctxs[] = {
-        [IDENT_DEFAULT]        = "variable",
-        [IDENT_FUNCTION]       = "function",
-        [IDENT_NAMESPACE]      = "namespace",
-        [IDENT_ENUMERATOR]     = "enumerator",
-        [IDENT_FUNCTION_PARAM] = "function param"
-    };
-    if (nnc_st_has(st, sym->name)) {
-        nnc_symbol* in_table_sym = nnc_st_get(st, sym->name);
-        if (sym->ictx == in_table_sym->ictx) {
+/**
+ * @brief Tryes to put symbol into specified `st`.
+ * @param st Pointer to `nnc_st` in which to perform inserting.
+ * @param sym Pointer to `nnc_sym` to be inserted. 
+ * @throw `NNC_NAME_ALREADY_DECLARED` in case when symbol with this name is already inserted.
+ */
+nnc_static void nnc_st_put_sym(nnc_st* st, nnc_sym* sym) {
+    nnc_sym* st_sym = nnc_st_get(st, sym->name, ST_SYM_IDENT);
+    if (st_sym != NULL) {
+        if (sym->ictx == st_sym->ictx) {
             THROW(NNC_NAME_ALREADY_DECLARED, sformat("%s \'%s\' is already declared.",
-                ctxs[in_table_sym->ictx], in_table_sym->name), sym->ctx);
+                ictx_str[st_sym->ictx], st_sym->name), sym->ctx);
         }
         else {
             THROW(NNC_NAME_ALREADY_DECLARED, sformat("cannot declare %s, %s with name \'%s\' is already declared.", 
-                ctxs[sym->ictx], ctxs[in_table_sym->ictx], in_table_sym->name), sym->ctx);
+                ictx_str[sym->ictx], ictx_str[st_sym->ictx], st_sym->name), sym->ctx);
         }
     }
     map_put_s(st->syms, sym->name, sym);
 }
 
-void nnc_st_put_type(nnc_st* st, nnc_type* type) {
+/**
+ * @brief Tryes to put type into specified `st`.
+ * @param st Pointer to `nnc_st` in which to perform inserting.
+ * @param type Pointer to `nnc_type` to be inserted. 
+ * @throw `NNC_TYPE_ALREADY_DECLARED` in case when type with this name is already inserted.
+ */
+nnc_static void nnc_st_put_type(nnc_st* st, nnc_type* type) {
     assert(type->kind == T_ALIAS);
     if (nnc_st_has_type(st, type->repr)) {
         THROW(NNC_TYPE_ALREADY_DECLARED, sformat("type \'%s\' is already declared.", nnc_type_tostr(type)));
@@ -82,6 +122,29 @@ void nnc_st_put_type(nnc_st* st, nnc_type* type) {
     map_put_s(st->types, type->repr, type);
 }
 
+/**
+ * @brief Tryes to put generic symbol into specified `st`.
+ * @param st Pointer to `nnc_st` in which to perform inserting.
+ * @param sym Pointer to generic symbol to be inserted. 
+ * @param kind Kind of symbol.
+ */
+void nnc_st_put(nnc_st* st, nnc_heap_ptr sym, nnc_st_sym_kind kind) {
+    switch (kind) {
+        case ST_SYM_TYPE:  nnc_st_put_type(st, (nnc_type*)sym); break;
+        case ST_SYM_IDENT: nnc_st_put_sym(st, (nnc_sym*)sym);   break;
+        default: nnc_abort_no_ctx("nnc_st_put: unknown sym kind.");
+    }
+}
+
+/**
+ * @brief Determines if `st` or it's parents has some context.
+ *  Optionally may return some symtable which has specified context.
+ * @param st Pointer to `st` from which to start. 
+ * @param t_st Pointer to preallocated variable in which optionally matched symtable can be stored.
+ *  To ignore this value, NULL can be passed.
+ * @param ctx Symtable's context to search.
+ * @return `true` if found symtable with such context, `false` otherwise.
+ */
 nnc_bool nnc_st_has_ctx(const nnc_st* st, nnc_st** t_st, nnc_st_ctx ctx) {
     nnc_bool has_ctx = false;
     for (; st != NULL && !has_ctx; st = st->root) {
--- src/nnc_symtable.h
+++ src/nnc_symtable.h
@@ -1,15 +1,18 @@
 #ifndef _NNC_SYMTABLE_H
 #define _NNC_SYMTABLE_H
 
-#include "nnc_ast.h"
-#include "nnc_literal.h"
 #include "nnc_map.h"
 
-typedef nnc_ident nnc_symbol;
-typedef struct _nnc_type nnc_type;
+#include "nnc_ast.h"
+#include "nnc_literal.h"
 
-typedef struct _nnc_fn_statement nnc_fn_statement;
-typedef struct _nnc_namespace_statement nnc_namespace_statement;
+typedef struct _nnc_type            nnc_type;
+typedef struct _nnc_fn_statement    nnc_fn_statement;
+
+typedef enum _nnc_st_sym_kind {
+    ST_SYM_TYPE,
+    ST_SYM_IDENT
+} nnc_st_sym_kind;
 
 typedef enum _nnc_st_ctx {
     ST_CTX_DEFAULT   = 0x00000000,
@@ -22,25 +25,31 @@ typedef enum _nnc_st_ctx {
     ST_CTX_ELSE      = 0x01000000
 } nnc_st_ctx;
 
+typedef nnc_ident nnc_sym;
+
 typedef struct _nnc_st {
     nnc_st_ctx ctx;
     struct _nnc_st* root;
     struct _nnc_st** branches;
     _map_(nnc_str, nnc_type*) types;
-    _map_(nnc_str, nnc_symbol*) syms;
+    _map_(nnc_str, nnc_sym*) syms;
     struct _nnc_st_ref {
         nnc_fn_statement* fn;
     } ref;
 } nnc_st;
 
-void nnc_st_init(nnc_st* out_table);
-void nnc_st_put(nnc_st* st, nnc_symbol* sym);
-void nnc_st_put_type(nnc_st* st, nnc_type* type);
-nnc_bool nnc_st_has(const nnc_st* st, const char* key);
-nnc_bool nnc_st_has_type(const nnc_st* st, const char* key);
-nnc_symbol* nnc_st_get(const nnc_st* st, const char* key);
-nnc_type* nnc_st_get_type(const nnc_st* st, const char* key); 
-nnc_symbol* nnc_st_get_below(const nnc_st* st, const char* key);
+void nnc_st_init(nnc_st* out_st);
+void nnc_st_fini(nnc_st* st);
+void nnc_st_put(nnc_st* st, nnc_heap_ptr sym, nnc_st_sym_kind kind);
+
+#define nnc_st_has_sym(st, key)    nnc_st_has(st, key, ST_SYM_IDENT)
+#define nnc_st_has_type(st, key)   nnc_st_has(st, key, ST_SYM_TYPE)
+nnc_bool nnc_st_has(const nnc_st* st, const char* key, nnc_st_sym_kind kind);
+
+#define nnc_st_get_sym(st, sym)     (nnc_sym*)nnc_st_get(st, sym, ST_SYM_IDENT)
+#define nnc_st_get_type(st, type)   (nnc_type*)nnc_st_get(st, type, ST_SYM_TYPE)
+nnc_heap_ptr nnc_st_get(const nnc_st* st, const char* key, nnc_st_sym_kind kind);
+
 nnc_bool nnc_st_has_ctx(const nnc_st* st, nnc_st** t_st, nnc_st_ctx ctx);
 
 #endif
