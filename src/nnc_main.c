67675767b5deff5e2904105b03d810105ba0af44
almeswe <armag2003@mail.ru>
2024-04-13 21:48:52 +0300

    added automated compilation with 'as' and 'ld' inside nnc_main.c

--- src/nnc_main.c
+++ src/nnc_main.c
@@ -29,9 +29,50 @@ static void nnc_stop_count_time() {
     //fprintf(stderr, "[elapsed: %.3lfms]\n", elapsed); 
 }
 
+static FILE* nnc_create_file(const char* path) {
+    FILE* file = fopen(path, "w+");
+    if (file == NULL) {
+        nnc_abort_no_ctx(sformat("cannot create `%s`. "
+            "[errno %d] %s.\n", path, errno, strerror(errno)));
+    }
+    return file;
+}
+
+static void nnc_write_blob(const char* path, FILE* fp, const nnc_blob_buf* blob) {
+    size_t written = fwrite(blob->blob, 1, blob->len, fp);
+    if (written != blob->len || errno != 0) {
+        nnc_abort_no_ctx(sformat("cannot write data to `%s`. "
+            "[errno %d] %s.\n", path, errno, strerror(errno)));
+    }
+}
+
+static void nnc_compile(const nnc_assembly_file* asm_file) {
+    char dot_s_file[MAX_PATH] = {0};
+    char dot_o_file[MAX_PATH] = {0};
+    sprintf(dot_s_file, "%s.s", glob_nnc_argv.output);
+    sprintf(dot_o_file, "%s.o", glob_nnc_argv.output);
+    // create output file and indicate if it is not possible
+    FILE* out = nnc_create_file(glob_nnc_argv.output);
+    fclose(out);
+    FILE* dot_o = nnc_create_file(dot_o_file);
+    fclose(dot_o);
+    FILE* dot_s = nnc_create_file(dot_s_file);
+    nnc_blob_buf asm_blob = nnc_build(*asm_file);
+    nnc_write_blob(dot_s_file, dot_s, &asm_blob);
+    fclose(dot_s);
+    system(sformat("as --64 -o %s %s", dot_o_file, dot_s_file));
+    system(sformat("ld -o %s %s", glob_nnc_argv.output, dot_o_file));
+    remove(dot_s_file);
+    remove(dot_o_file);
+}
+
 static nnc_i32 nnc_main(nnc_i32 argc, char* argv[]) {
     TRY {
         nnc_parse_argv(argc, argv);
+        assert(buf_len(glob_nnc_argv.sources) == 1);
+        assert(buf_len(glob_nnc_argv.objects) == 0);
+        assert(buf_len(glob_nnc_argv.static_libs) == 0);
+        assert(buf_len(glob_nnc_argv.shared_libs) == 0);
         glob_current_ast = nnc_parse(glob_nnc_argv.sources[0]);
         nnc_check_for_errors();
         nnc_resolve(glob_current_ast);
@@ -42,8 +83,8 @@ static nnc_i32 nnc_main(nnc_i32 argc, char* argv[]) {
         if (glob_nnc_argv.dump_ir) {
             nnc_dump_3a_code(code);
         }
-        nnc_assembly_file out_asm = nnc_gen(code);
-        fprintf(stderr, "%s", nnc_build(out_asm).blob);
+        nnc_assembly_file unbuilt = nnc_gen(code);
+        nnc_compile(&unbuilt);
         ETRY;
     }
     CATCHALL {
--- src/py_test/compile.sh
+++ src/py_test/compile.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-./nnc $@ 2> bootstrap.s 
-gcc -g -c bootstrap.s -o compiled.o
-ld compiled.o -o compiled 
+./nnc $@ -o compiled 
