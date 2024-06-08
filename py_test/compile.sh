f21241f31d62c9064abc45f96127d96ecd482530
almeswe <armag2003@mail.ru>
2024-06-08 14:05:30 +0300

    python automatic tests updated

--- py_test/compile.sh
+++ py_test/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./nnc --no-opt $@ libnnc_deps_x86_64.a -o compiled 
+nnc --no-opt $@ libnnc_deps_x86_64.a rt.o -o compiled 
--- py_test/prepare.sh
+++ py_test/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cp ../src/build/nnc .
+cp ../src/build/rt.o .
 cp ../src/build/libnnc_deps_x86_64.a .
